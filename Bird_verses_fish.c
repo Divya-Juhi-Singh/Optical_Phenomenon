e7d013c633adb72786495d760d6f642ae3b4510c
Divya Juhi Singh <57247846+Divya-Juhi-Singh@users.noreply.github.com>
2020-07-19 23:16:46 +0530

    Some correction in the formula used.

--- Bird_verses_fish.c
+++ Bird_verses_fish.c
@@ -32,7 +32,7 @@ int main(void)
      In reality, the fish is at a distance of %.3f m\n\
      away from the bird.\n",u);
  
-    v1 = ( n2 / n1 ) * u; // formula for apparent distance
+    v1 = ( n1 / n2 ) * u; // formula for apparent distance
 
     printf("\
      The bird locates the fish at a depth of %.3f m\n\
@@ -44,15 +44,15 @@ int main(void)
      In reality, the bird is a distance of %.3f m\n\
      away from the fish.\n", u);
 
-    v2 = ( n1 / n2 ) * u; // formula for apparent distance
+    v2 = ( n2 / n1 ) * u; // formula for apparent distance
 
     printf("\
      The fish locates the bird at a height of %.3f m\n\
      from itself.\n", v2);
  
     printf("\nConclusion:\n\n\
-     Bird locates a fish farther from the fish's real position\n\
-     while fish locates a bird nearer with respect to the\n\
+     Bird locates a fish nearer from the fish's real position\n\
+     while fish locates a bird farther with respect to the\n\
      bird's real position.\n");
  
     return 0;
